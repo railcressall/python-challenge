99d49e252f0e66e9307ede9a91bc279ecb8a1d5c
railcressall <155472716+railcressall@users.noreply.github.com>
2024-03-07 08:46:51 -0700

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -2,6 +2,7 @@ import csv
 
 # Specify the path to your CSV file
 # csv_file_path = "./resources/budget_data.csv"
+print("reading file")
 csv_file_path = r"C:\Users\railc\OneDrive\Desktop\python-challenge\pybank\resources\budget_data.csv"
 
 # Open the CSV file and read its content
